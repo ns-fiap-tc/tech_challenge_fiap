45aead34b092fceb1e92ab5f0012ebdb268609d2
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-05-31 23:25:21 -0300

    chore: add remote backend setup + add terraform destroy action for convinience

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "lanchonete-tfstate-bucket"
+    key            = "tech-challenge-app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
