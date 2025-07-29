af8c57cc65fdd7547ab6501e4c7209d966161e10
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-07-29 10:20:48 -0300

    Update deploy workflow and backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 # Remote backend instance, to save tfstate
 terraform {
   backend "s3" {
-    bucket         = "lanchonete-tfstate-bucket"
+    bucket         = "tc-lanchonete-tfstate-bucket"
     key            = "tech-challenge-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
